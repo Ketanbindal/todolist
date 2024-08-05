14d8d3e00536c8e7da738a5b65cc5be4de0cb265
Ketanbindal <ketanbindal@gmail.com>
2024-08-05 22:11:23 +0530

    frist commit

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+   document.addEventListener('DOMContentLoaded', ()=>
+{ 
+   document.title = "TO DO LIST ";
     const inp = document.getElementById('item');
     let addelement = document.getElementById('addbtn');
     let list = document.getElementById('list');
     addelement.addEventListener('click',() =>
         {
-
+            if(inp.value !== '')
+            {
+            let checkbox = document.createElement('span');
+            checkbox.className = "checkbox";
             console.log("clicked");
-            let elem = document.createElement('Li');
-            elem.textContent= inp.value;
-            list.appendChild(elem);
+            let elem = document.createElement('li');
+            let content = document.createElement('span');
+            content.textContent = inp.value;
+
+            elem.appendChild(checkbox);
+            elem.appendChild(content);
+            list.appendChild(elem);}
+            else
+            {
+                alert("please enter something");
+            }
+            inp.value = '';
         });
+    let chkbtn = document.getElementsByClassName()
+        
+    })
